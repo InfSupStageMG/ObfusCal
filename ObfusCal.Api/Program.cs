a4c1815999420d74a464821b5d1422f75c246255
Gijs <gijs.pennings@infosupport.com>
2026-03-31 16:57:04 +0200

    Minifix for builder lacking

--- ObfusCal.Api/Program.cs
+++ ObfusCal.Api/Program.cs
@@ -4,6 +4,8 @@ using ObfusCal.Core.Obfuscation.Transformers;
 using ObfusCal.Infrastructure.Calendars;
 using ObfusCal.Infrastructure.Peers;
 
+var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddScoped<ICalendarSource, MockCalendarSource>(); // swap to GraphCalendarSource later
 builder.Services.AddScoped<IEventTransformer, StripTitleTransformer>();
 builder.Services.AddScoped<IEventTransformer, RemoveAttendeesTransformer>();
