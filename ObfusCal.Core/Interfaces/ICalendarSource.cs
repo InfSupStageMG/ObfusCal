66876c05bf1ce4eb67aa7de7b35957f3ef0def70
Gijs <gijs.pennings@infosupport.com>
2026-04-20 11:00:41 +0200

    CalendarEvent added and tests added

---
+++ ObfusCal.Core/Interfaces/ICalendarSource.cs
@@ -0,0 +1,11 @@
+﻿using ObfusCal.Core.Models;
+
+namespace ObfusCal.Core.Interfaces;
+
+public interface ICalendarSource
+{
+    Task<IReadOnlyList<CalendarEvent>> GetEventsAsync(
+        DateTimeOffset from,
+        DateTimeOffset to,
+        CancellationToken ct = default);
+}
---
+++ ObfusCal.Core/Models/CalendarEvent.cs
@@ -0,0 +1,11 @@
+﻿namespace ObfusCal.Core.Models;
+
+public record CalendarEvent(
+    string Id,
+    string Title,
+    string? Description,
+    DateTimeOffset Start,
+    DateTimeOffset End,
+    IReadOnlyList<string> AttendeeEmails,
+    string? Location
+);
